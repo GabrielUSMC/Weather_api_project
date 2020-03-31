e4776d52ae5c9ddae42252c3bd182cfe3723cbaf
Vincent Heningburg <GabrielUSMC@gmail.com>
2020-03-31 15:52:30 -0700

    Final output data for WeatherPy

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d62d19700ab702cc74653a0e666ebbed"
 
 # Google API Key
 g_key = "AIzaSyCbJcb4i8q2E3G-qfdoTOZZjxjRupChCZw"
